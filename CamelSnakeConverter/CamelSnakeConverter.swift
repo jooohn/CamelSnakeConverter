eadf2668ead9242ec66f8eb4257da4507c699240
bloodysnowx <bloodysnowx@gmail.com>
2016-08-23 10:28:36 +0900

    fix

--- CamelSnakeConverter/CamelSnakeConverter.swift
+++ CamelSnakeConverter/CamelSnakeConverter.swift
@@ -1,12 +1,12 @@
 import Foundation
 
-extension String {
-    func toCamel(lower:Bool=true) -> String {
+public extension String {
+    public func toCamel(lower:Bool=true) -> String {
         let camel = capitalizedString.stringByReplacingOccurrencesOfString("(\\w{0,1})_", withString: "$1", options: .RegularExpressionSearch, range: nil)
         return lower ? camel.stringByReplacingCharactersInRange(camel.startIndex...camel.startIndex, withString: substringToIndex(self.startIndex.advancedBy(1)).lowercaseString) : camel
     }
     
-    func toSnake() -> String? {
+    public func toSnake() -> String? {
         let upperCased = stringByReplacingCharactersInRange(startIndex...startIndex, withString: substringToIndex(startIndex.advancedBy(1)).uppercaseString)
         guard let regex = try? NSRegularExpression(pattern: "[A-Z][a-z,\\d]+", options: []) else { return nil }
         let matches = regex.matchesInString(upperCased, options: [], range: NSRange(location: 0, length: upperCased.characters.count))
